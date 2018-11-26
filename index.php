db244eb965302ce7bb461a47eb4ce9ba00faf775
dosterz97 <dosterz97@gmail.com>
2018-11-26 16:42:53 -0600

    base set up for initing the database

--- index.php
+++ index.php
@@ -6,15 +6,14 @@
         <link rel="stylesheet" type="text/css" href="Styles/landing.css">
 	</head>
 	<body>
+    <?php
+        require 'initDB.php';
+    ?>
         <h1 class="header">
             <img class="image" src="images/BetaSigCrest.png" alt="Crest">
             Beta Sigma Psi
             <img class="image" src="images/BetaSigCrest.png" alt="Crest">
         </h1>
-        <?php 
-        require 'initDB.php';
-        ?>
-        
         <div class="wrapper">
             <button type="button" class="buttons" onclick="window.location.href='message.php'">Message</button>
         </div>
@@ -36,25 +35,24 @@
             </div>-->
         </div>
 	</body>
-
     <script>
-        function myFunction() {
-            document.getElementById("myDropdown").classList.toggle("show");
-        }
+            function myFunction() {
+                document.getElementById("myDropdown").classList.toggle("show");
+            }
 
-        function filterFunction() {
-            var input, filter, ul, li, a, i;
-            input = document.getElementById("myInput");
-            filter = input.value.toUpperCase();
-            div = document.getElementById("myDropdown");
-            a = div.getElementsByTagName("a");
-            for (i = 0; i < a.length; i++) {
-                if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    a[i].style.display = "";
-                } else {
-                    a[i].style.display = "none";
+            function filterFunction() {
+                var input, filter, ul, li, a, i;
+                input = document.getElementById("myInput");
+                filter = input.value.toUpperCase();
+                div = document.getElementById("myDropdown");
+                a = div.getElementsByTagName("a");
+                for (i = 0; i < a.length; i++) {
+                    if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                        a[i].style.display = "";
+                    } else {
+                        a[i].style.display = "none";
+                    }
                 }
             }
-        }
-    </script>
+        </script>
 </html>
--- initDB.php
+++ initDB.php
@@ -1,8 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+function makeQuery($myConnection, $myQuery) {
+    if($myConnection->query() === TRUE) {
+        logVariableWithContext("Successful query", $myQuery);
+    }
+    else if($myConnection->error) {
+        logVariableWithContext("Error in query", $myConnection->error);
+    }
+}
 
+function logVariable($var) {
+    ?><script>console.log("<?php echo $var?>");</script><?php
+}
+function logVariableWithContext($context, $var) {
+    ?><script>console.log("<?php $context . $var?>");</script><?php
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbName = "beta_sig_iota";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 // Check connection
@@ -11,12 +27,29 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-// Create database
-$sql = "CREATE DATABASE IF NOT EXISTS beta_sig_iota";
-if ($conn->query($sql) !== TRUE) {
-    ?><script>console.log("Error creating database: " . $conn->error);</script><?php
-}
-?><script>console.log("Database Up and Running.")</script><?php
+// Create database if it doesn't exist
+$createDB = "CREATE DATABASE IF NOT EXISTS " . $dbName;
+makeQuery($conn,$createDB);
 
+if ($conn->query($createDB) !== TRUE) {
+    logVariableWithContext("Error in creating database", $conn->error);
+}
+logVariable("Database Up and Running");
 $conn->close();
+
+$conn = new mysqli($servername, $username, $password, $dbName);
+//Create tables
+$member = "CREATE TABLE IF NOT EXISTS Member(
+    pawprint varchar(6) PRIMARY KEY,
+    fName varchar(30),
+    lName varchar(30),
+    semesterJoined varchar(30),
+    mStatus ENUM ('active', 'inactive', 'associate', 'alumni'),
+    schoolYear ENUM ('freshman', 'sophmore', 'junior', 'senior'),
+    roomNumber int,
+    FOREIGN KEY (roomNumber) REFERENCES Room(roomID)
+  )";
+makeQuery($conn,$member);
+
+
 ?>
