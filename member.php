4099a06dfddff6df589e8d060fa7555baf3b0ac0
Kyle <kbmeyer1997@yahoo.com>
2018-11-29 18:34:54 -0600

    Basic outline for member page display information

--- member.php
+++ member.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Beta Sigma Psi - Member</title>
         <link rel="stylesheet" type="text/css" href="Styles/landing.css">
-        <link rel="stylesheet" type="text/css" href="Styles/member.css">
+        <link rel="stylesheet" type="text/css" href="Styles/tables.css">
     </head>
     <body>
         <h1 class="header">
@@ -12,12 +12,20 @@
             Beta Sigma Psi - Member
             <img class="image" src="images/BetaSigCrest.png" alt="Crest">
         </h1>
+        <h2 id="nameBox" style="text-align: center">
+            Jack Allen
+        </h2>        
         <div id="memberPage">
-            <button type="button" class="Sbuttons" onclick="window.location.href='FinesPage.php'">Fines</button><br>
+            <button type="button" class="Sbuttons" onclick="window.location.href='FinesPage.php'">Fines</button>
             <button type="button" class="Sbuttons" onclick="window.location.href='WorkOrders.php'">Work Orders</button>
         </div>
-        <div style="text-align: center">
-            Text
+        <div>
+            Semester Joined: <br>
+            Pawprint: <br>
+            Current Room: <br>
+            Status: <br>
+            Senority Points: <br>
+            Position(s) Held: <br>
         </div>
     </body>
 </html>
