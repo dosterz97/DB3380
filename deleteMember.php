79a82d56afa021d87fea0cb89d85034247afa225
Kyle <kbmeyer1997@yahoo.com>
2018-12-02 17:42:55 -0600

    deleteMember.php now works and can delete member from the database

--- deleteMember.php
+++ deleteMember.php
@@ -1,29 +1,7 @@
 <?php 
     session_start();
-    function makeQuery($myConnection, $myQuery) {
-    $result = $myConnection->query($myQuery);
-    if($result === TRUE) {
-        logVariable("Successful query");
-    }
-    else if($result->error) {
-        logVariableWithContext("Error in query: ", $myConnection->error);
-    }
-    else {
-        logVariable("query didn't return TRUE or an error");
-        logVariable($result);
-    }
-}
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "beta_sig_iota";
 
-    $conn = new mysqli($servername, $username, $password);
-
-    if($conn->connect_error) {
-        logVariableWithContext("Error connecting to database: ", $conn->connect_error);
-        die("Connection failed: " . $conn->connect_error);
-    }
+    require 'initDB.php';
 
     $pawprint = $_SESSION['pawprint'];
     $deleteQuery  = "DELETE FROM Member WHERE pawprint = '$pawprint';";
