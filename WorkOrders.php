970eb734b8c49190e62e27bf61458efbce15d328
Jack Allen <jackykrabs@gmail.com>
2018-12-01 21:23:34 -0600

    Fix add member and filter work orders

--- WorkOrders.php
+++ WorkOrders.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,6 +11,9 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     </head>
     <body>
+        <?php
+            require 'initDB.php';
+        ?>
         <h1 class="header">
             <img class="image" src="images/BetaSigCrest.png" alt="Crest">
             Beta Sigma Psi - Work Orders
@@ -26,6 +32,17 @@
                 </tr>
                 </thead>
                 <tbody>
+                    <?php
+                        $pawprint = $_SESSION['pawprint'];
+                        $sql = "SELECT * FROM WorkOrder WHERE WorkOrder.filingMemberId = '$pawprint'";
+                        $result = mysqli_query($conn, $sql);
+                        while($workOrder = mysqli_fetch_assoc($result)) {
+                            echo '<tr>';
+                            echo '<td>'.$workOrder['id'].'</td><td>'.$workOrder['location'].'</td><td>'.$workOrder['workOrderDescription'].'</td><td>'.$workOrder['amount'].'</td><td>'.$workOrder['workOrderStatus'].'</td>';
+                            echo '</tr>';
+                        }
+        
+                    ?>
                 </tbody>
             </table>
         </div>
